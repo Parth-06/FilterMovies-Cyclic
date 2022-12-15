caee52d68db89f6a813bf43be2245411bffa5e57
Parth-06 <parth99.thakkar@gmail.com>
2022-12-15 15:35:29 +0530

    first commit

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 
-/** base url to make requests to the the movie database*/
 const config = {
-  headers: { "Access-Control-Allow-Origin": "*", "Content-Type": "text/plain" },
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Content-Type": "text/plain",
+  },
 };
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   https: config,
 });
-// const instance=axios.create({
-//   baseURL:"https://api.themoviedb.org/3",
-// })
 
 export default instance;
